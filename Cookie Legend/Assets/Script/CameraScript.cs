8d562efb2211d687a470c17c1136e03748435d4f
MrChuckchucky <b.devienne@rubika-edu.com>
2016-04-26 09:36:29 +0200

    Pause/Play

---
+++ Cookie Legend/Assets/Script/CameraScript.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraScript : MonoBehaviour
+{
+    GameObject cookie;
+    public Vector3[] positions;
+	// Use this for initialization
+	void Start ()
+    {
+        cookie = GameObject.FindGameObjectWithTag("Cookie");
+        positions = new Vector3[10];
+        for(int i = 0; i < positions.Length; i++)
+        {
+            positions[i] = cookie.transform.position;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if(cookie.GetComponent<CookieManager>().playMode)
+        {
+            for (int i = positions.Length - 1; i > 0; i--)
+            {
+                positions[i] = positions[i - 1];
+            }
+            positions[0] = cookie.transform.position;
+            transform.Translate(new Vector3((positions[positions.Length - 1].x - transform.position.x), (positions[positions.Length - 1].y - transform.position.y), positions[positions.Length - 1].z));
+        }
+        else
+        {
+            for (int i = 0; i < positions.Length; i++)
+            {
+                positions[i] = cookie.transform.position;
+            }
+        }
+	}
+}
---
+++ Cookie Legend/Assets/Script/GameManager.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    Transform[] myObjects;
+    Vector3[] myPositions;
+    GameObject cookie;
+    Vector3 initPos;
+
+    public GameObject pauseButton;
+    // Use this for initialization
+    void Start ()
+    {
+        cookie = GameObject.FindGameObjectWithTag("Cookie");
+        initPos = cookie.transform.position;
+	}
+	// Update is called once per frame
+	void Update ()
+    {
+	}
+
+    public void Pause()
+    {
+        if (cookie.transform.position != initPos)
+        {
+            GameObject.FindGameObjectWithTag("Cookie").GetComponent<CookieManager>().playMode = false;
+            pauseButton.GetComponentInChildren<Text>().text = "Play";
+            for (int i = 0; i < myObjects.Length; i++)
+            {
+                myObjects[i].transform.position = myPositions[i];
+                if (myObjects[i].tag == "Cookie")
+                {
+                    cookie.GetComponent<CookieManager>().gravity = 10;
+                    cookie.GetComponent<CookieManager>().vitesseMax = 10;
+                    cookie.GetComponent<CookieManager>().vitesse = 0;
+                    cookie.GetComponent<CookieManager>().acceleration = 20;
+                    cookie.GetComponent<CookieManager>().height = 0;
+                    cookie.GetComponent<CookieManager>().runStart = 0;
+                    cookie.GetComponent<CookieManager>().direction = 1;
+
+                    cookie.GetComponent<CookieManager>().isLanded = false;
+                    cookie.GetComponent<CookieManager>().alreadyLanded = false;
+                    cookie.GetComponent<CookieManager>().isWalled = false;
+                    cookie.GetComponent<CookieManager>().alreadyWalled = false;
+                    cookie.GetComponent<CookieManager>().isTurning = false;
+                    cookie.GetComponent<CookieManager>().inSugar = false;
+                    cookie.GetComponent<CookieManager>().dead = false;
+                    cookie.transform.localScale = new Vector3(1, 1, 1);
+                }
+            }
+        }
+        else
+        {
+            GameObject.FindGameObjectWithTag("Cookie").GetComponent<CookieManager>().playMode = true;
+            pauseButton.GetComponentInChildren<Text>().text = "Stop";
+            myObjects = GameObject.FindObjectsOfType<Transform>();
+            myPositions = new Vector3[myObjects.Length];
+            for(int i = 0; i < myObjects.Length; i++)
+            {
+                myPositions[i] = myObjects[i].position;
+            }
+        }
+    }
+}
